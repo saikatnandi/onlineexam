bb75e26dfe8422a318373a333c71299fb21cb0b1
alamin <mdabdullahalalaminp@gmail.com>
2016-05-25 19:03:20 +0600

    Routine Update

--- mysite/git bash.sh
+++ mysite/git bash.sh
@@ -1,8 +1,6 @@
 git add -A .
 git commit -m "Routine Update"
 git push -u origin master
-expect "Username for 'https://github.com': "
-send "mdabdullahalalaminp@gmail.com"
 
 echo e "\n\n\n\n"
 read -p "********* Uploaded to GitHub Finished ********" a
